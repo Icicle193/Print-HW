23d8af3f98671d89f4c2978d9e436d06432b1f4f
Icicle193 <nevaankumar2015@gmail.com>
2025-07-11 21:08:31 -0400

    Add files via upload

---
+++ prog3.py
@@ -0,0 +1,7 @@
+print("My name is Nevaan")
+
+name = input ("What is your name:")
+print("Hello", name)
+
+print("You should learn to code at codingal")
+print("You learn about search engines to making games and websites so join codingal")
